eda362c01fb8fe46edb58cf38b74147470473947
“Varvara” <berestenvarvara@gmail.com>
2025-07-25 10:05:34 +0300

    pagination

--- src/main.js
+++ src/main.js
@@ -46,6 +46,7 @@ async function render(action) {
   query = applySearching(query, state, action);
   query = applyFiltering(query, state, action);
   query = applySorting(query, state, action);
+  query = applyPagination(query, state, action);
   const { total, items } = await api.getRecords(query);
   updatePagination(total, query);
   sampleTable.render(items);
